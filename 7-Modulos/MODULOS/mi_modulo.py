a638817558b0dd140b449485a224ff4e190f3ea0
marianoperezm <perezmariano5101@gmail.com>
2023-11-22 17:56:37 -0300

    creo carpeta y agrego archivo de modulos

---
+++ 7-Modulos/MODULOS/mi_modulo.py
@@ -0,0 +1,12 @@
+"""
+Modulo con funciones propias
+"""
+
+def saludar():
+    print("Hola, soy una función en mi_modulo.py")
+
+VELOCIDAD_LUZ = 299792458
+
+if __name__ == '__main__':
+    print("Codigo ejecutado desde mi_modulo.py")
+    saludar()
